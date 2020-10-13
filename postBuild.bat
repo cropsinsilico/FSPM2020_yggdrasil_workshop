aa43209457f1185b0c91eae8d83cf9f04c212245
Meagan Lang <langmm.astro@gmail.com>
2020-10-13 13:15:13 -0700

    Remove yggdrasil as a submodule.

--- postBuild.bat
+++ postBuild.bat
@@ -1,7 +1,3 @@
-cd yggdrasil
-python utils/install_from_requirements.py conda requirements.txt requirements_condaonly.txt
-python setup.py develop
-cd ../
 yggconfig
 python -c "import yggdrasil"
 ygginfo
